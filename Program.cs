e2fc5cc48e40ce97e7aad2dcd35f7d389e548fd8
Mateus Cerqueira <mateus.mcg@gmail.com>
2018-06-20 21:53:38 -0300

    Alterando porta que a API irá utilizar

--- Program.cs
+++ Program.cs
@@ -14,7 +14,10 @@ namespace livraria_virtual_auth_api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .UseUrls("http://localhost:8002")
+                .Build()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
